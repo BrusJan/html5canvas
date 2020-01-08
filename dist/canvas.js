2a3ea6040f37fe1ac8d5715954ce99e59decd42c
Jan Brus <jan.brus@onsemi.com>
2020-01-08 09:50:34 +0100

    images edited, zoom, goto page

--- dist/canvas.js
+++ dist/canvas.js
@@ -1,40 +1,11 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+var DrawnObject = /** @class */ (function () {
+    function DrawnObject(obj, pageNumber) {
+        this.obj = obj;
+        this.pageNumber = pageNumber;
     }
-};
+    return DrawnObject;
+}());
 var Point = /** @class */ (function () {
     function Point(x, y) {
         this.x = x;
@@ -66,41 +37,48 @@ var MAX_BRUSH_POINT_COUNT = 100;
 var cvWidth = 600;
 var cvHeight = 800;
 var tool = 0; // 0 = none, 1 = line, 2 = text
+var version = 2; // 1 = original, 2 = my edit, 3 = solution
+var zoom = 1;
 var drawnObjects = new Array();
 var newLine = new Line(new Boundaries(new Point(0, 0), new Point(0, 0)), false);
 var brushIsDrawing = false;
 var brushPoints = new Array();
 var brushLastPoint = new Point(0, 0);
 var brushCurrentPoint = new Point(0, 0);
-var imgOrig = new Image();
+var image = new Image();
 var imgDone = new Image();
 var imgUsr = new Image();
 var pageNumber = 1;
-function lineClick() {
-    setTool(1);
-}
-function brushClick() {
-    setTool(2);
-}
-function textClick() {
-    setTool(3);
-    console.info({ drawnObjects: drawnObjects });
-}
 function setTool(t) {
     tool = t;
+}
+function zoomCanvas(z) {
+    zoom += z;
     var ispan = document.getElementById('i-tool');
     if (ispan)
-        ispan.innerHTML = t.toString();
+        ispan.innerHTML = 'zoom ' + zoom.toString();
 }
 window.onload = function () {
     setTool(0);
+    setImgSrc();
     var cv = document.getElementById('canvas');
     var ctx = cv.getContext("2d");
     var inputPageNumber = document.getElementById('inputPageNumber');
+    var btnPrevPage = document.getElementById('btnPrevPage');
+    var btnNextPage = document.getElementById('btnNextPage');
+    var lblSwitch1 = document.getElementById('lblSwitch1');
+    var lblSwitch2 = document.getElementById('lblSwitch2');
+    var lblSwitch3 = document.getElementById('lblSwitch3');
+    inputPageNumber.value = pageNumber.toString();
     cv.addEventListener("mousedown", handleMouseDown);
     cv.addEventListener("mouseup", handleMouseUp);
     cv.addEventListener("mousemove", handleMouseMove);
-    this.imgOrig.src = 'img/' + pageNumber.toString() + '.png';
+    // first call, calls request animation frame inside co it cycles inside after this one call
+    redrawCanvas();
+    inputPageNumber.addEventListener("change", function (event) {
+        pageNumber = +inputPageNumber.value;
+        setImgSrc();
+    });
     inputPageNumber.addEventListener("keyup", function (event) {
         console.info('input click');
         // Number 13 is the "Enter" key on the keyboard
@@ -109,65 +87,51 @@ window.onload = function () {
             console.info('input enter click');
             event.preventDefault();
             pageNumber = +inputPageNumber.value;
-            redrawCanvas();
         }
+    });
+    lblSwitch1.addEventListener("click", function (event) {
+        switchVersion(1);
+    });
+    lblSwitch2.addEventListener("click", function (event) {
+        switchVersion(2);
+    });
+    lblSwitch3.addEventListener("click", function (event) {
+        switchVersion(3);
     });
     // button prev page click must be async to redraw image after a small delay, otherwise it does not redraw
     function prevPage() {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        cv.focus();
-                        pageNumber = --pageNumber;
-                        imgOrig.src = 'img/' + pageNumber.toString() + '.png';
-                        console.info('page: ' + pageNumber);
-                        // sleep for  10ms so the picture redraws
-                        return [4 /*yield*/, new Promise(function (r) { return setTimeout(r, 50); })];
-                    case 1:
-                        // sleep for  10ms so the picture redraws
-                        _a.sent();
-                        redrawCanvas();
-                        inputPageNumber.value = pageNumber.toString();
-                        return [2 /*return*/];
-                }
-            });
-        });
+        pageNumber = --pageNumber;
+        if (pageNumber < 1) {
+            pageNumber = 1;
+            btnPrevPage.disabled = true;
+        }
+        setImgSrc();
+        inputPageNumber.value = pageNumber.toString();
     }
     var prevbtn = document.getElementById('btnPrevPage');
     if (prevbtn)
         prevbtn.onclick = prevPage;
     // button next page click must be async to redraw image after a small delay, otherwise it does not redraw
     function nextPage() {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        cv.focus();
-                        pageNumber = ++pageNumber;
-                        imgOrig.src = 'img/' + pageNumber.toString() + '.png';
-                        console.info('page: ' + pageNumber);
-                        // sleep for  10ms so the picture redraws
-                        return [4 /*yield*/, new Promise(function (r) { return setTimeout(r, 50); })];
-                    case 1:
-                        // sleep for  10ms so the picture redraws
-                        _a.sent();
-                        redrawCanvas();
-                        inputPageNumber.value = pageNumber.toString();
-                        return [2 /*return*/];
-                }
-            });
-        });
+        pageNumber = ++pageNumber;
+        if (pageNumber == 2) {
+            btnPrevPage.disabled = false;
+        }
+        setImgSrc();
+        inputPageNumber.value = pageNumber.toString();
     }
     var nextbtn = document.getElementById('btnNextPage');
     if (nextbtn)
         nextbtn.onclick = nextPage;
     function handleMouseDown(e) {
+        // do stuff only when version 2 is active (moje upravy)
+        if (version != 2)
+            return;
         var rect = cv.getBoundingClientRect();
         switch (tool) {
             case 0: return;
             case 1:
-                newLine = new Line(new Boundaries(new Point(e.clientX - rect.left, e.clientY - rect.top), new Point(0, 0)), true);
+                newLine = new Line(new Boundaries(new Point(e.clientX - rect.left, e.clientY - rect.top), new Point(e.clientX - rect.left, e.clientY - rect.top)), true);
                 break;
             case 2:
                 brushPoints.push(new Point(e.clientX - rect.left, e.clientY - rect.top));
@@ -178,20 +142,26 @@ window.onload = function () {
         }
     }
     function handleMouseUp(e) {
+        // do stuff only when version 2 is active (moje upravy)
+        if (version != 2)
+            return;
         switch (tool) {
             case 0: return;
             case 1:
                 var rect = cv.getBoundingClientRect();
-                newLine.bo.b.x = e.clientX - rect.left;
-                newLine.bo.b.y = e.clientY - rect.top;
+                // recalculate original point
+                newLine.bo.a.x /= zoom;
+                newLine.bo.a.y /= zoom;
+                // set end point
+                newLine.bo.b.x = (e.clientX - rect.left) / zoom;
+                newLine.bo.b.y = (e.clientY - rect.top) / zoom;
                 newLine.editing = false;
                 // if line is too short, do not create it
                 if (Math.abs(newLine.bo.a.x - newLine.bo.b.x) > 3 || Math.abs(newLine.bo.a.y - newLine.bo.b.y) > 3) {
                     // object assign to create new object and not use the same refference over and over
-                    drawnObjects.push(newLine);
+                    drawnObjects.push(new DrawnObject(newLine, pageNumber));
                     newLine = new Line(new Boundaries(new Point(0, 0), new Point(0, 0)), false);
                 }
-                redrawCanvas();
                 break;
             case 2:
                 if (brushIsDrawing)
@@ -200,13 +170,15 @@ window.onload = function () {
         }
     }
     function handleMouseMove(e) {
+        // do stuff only when version 2 is active (moje upravy)
+        if (version != 2)
+            return;
         var rect = cv.getBoundingClientRect();
         switch (tool) {
             case 0: return;
             case 1:
                 newLine.bo.b.x = e.clientX - rect.left;
                 newLine.bo.b.y = e.clientY - rect.top;
-                redrawCanvas();
                 break;
             case 2:
                 if (!brushIsDrawing)
@@ -220,68 +192,93 @@ window.onload = function () {
                 }
                 if (brushPoints.length >= MAX_BRUSH_POINT_COUNT)
                     finishBrushStroke();
-                redrawCanvas();
                 break;
         }
     }
     function redrawCanvas() {
         if (!ctx)
             return;
+        cv.width = image.width * zoom;
+        cv.height = image.height * zoom;
         ctx.clearRect(0, 0, cv.width, cv.height);
-        ctx.drawImage(imgOrig, 0, 0, cv.width, cv.height);
+        ctx.drawImage(image, 0, 0, cv.width, cv.height);
         ctx.lineCap = "round";
         ctx.lineJoin = "round";
-        // draw objects
-        drawnObjects.forEach(function (object) {
-            if (object instanceof Line) {
-                ctx.lineWidth = 2;
-                ctx.strokeStyle = "black";
-                ctx.beginPath();
-                ctx.moveTo(object.bo.a.x, object.bo.a.y);
-                ctx.lineTo(object.bo.b.x, object.bo.b.y);
+        // draw all objects only if version 2 is active (moje upravy)
+        if (version == 2) {
+            // draw objects
+            drawnObjects.forEach(function (object) {
+                if (object.pageNumber != pageNumber)
+                    return;
+                if (object.obj instanceof Line) {
+                    ctx.lineWidth = 2 * zoom;
+                    ctx.strokeStyle = "black";
+                    ctx.beginPath();
+                    ctx.moveTo(object.obj.bo.a.x * zoom, object.obj.bo.a.y * zoom);
+                    ctx.lineTo(object.obj.bo.b.x * zoom, object.obj.bo.b.y * zoom);
+                    ctx.stroke();
+                    ctx.closePath();
+                }
+                else if (object.obj instanceof BrushStroke) {
+                    ctx.lineWidth = 10 * zoom;
+                    ctx.strokeStyle = "red";
+                    ctx.beginPath();
+                    ctx.moveTo(object.obj.points[0].x * zoom, object.obj.points[0].y * zoom);
+                    // i = index of single point in a brush stroke
+                    for (var i = 1; i < object.obj.points.length; i++) {
+                        ctx.lineTo(object.obj.points[i].x * zoom, object.obj.points[i].y * zoom);
+                        ctx.stroke();
+                    }
+                    ctx.closePath();
+                }
+            });
+            // draw current line
+            ctx.lineWidth = 2 * zoom;
+            ctx.strokeStyle = "black";
+            ctx.beginPath();
+            if (newLine.editing) {
+                ctx.moveTo(newLine.bo.a.x, newLine.bo.a.y);
+                ctx.lineTo(newLine.bo.b.x, newLine.bo.b.y);
                 ctx.stroke();
-                ctx.closePath();
             }
-            else if (object instanceof BrushStroke) {
-                ctx.lineWidth = 10;
-                ctx.strokeStyle = "red";
+            // draw current brush
+            ctx.lineWidth = 10 * zoom;
+            ctx.strokeStyle = "red";
+            if (brushIsDrawing) {
                 ctx.beginPath();
-                ctx.moveTo(object.points[0].x, object.points[0].y);
+                ctx.moveTo(brushPoints[0].x, brushPoints[0].y);
                 // i = index of single point in a brush stroke
-                for (var i = 1; i < object.points.length; i++) {
-                    ctx.lineTo(object.points[i].x, object.points[i].y);
+                for (var i = 1; i < brushPoints.length; i++) {
+                    ctx.lineTo(brushPoints[i].x, brushPoints[i].y);
                     ctx.stroke();
                 }
                 ctx.closePath();
             }
-        });
-        // draw current line
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = "black";
-        ctx.beginPath();
-        if (newLine.editing) {
-            ctx.moveTo(newLine.bo.a.x, newLine.bo.a.y);
-            ctx.lineTo(newLine.bo.b.x, newLine.bo.b.y);
-            ctx.stroke();
         }
-        // draw current brush
-        ctx.lineWidth = 10;
-        ctx.strokeStyle = "red";
-        if (brushIsDrawing) {
-            ctx.beginPath();
-            ctx.moveTo(brushPoints[0].x, brushPoints[0].y);
-            // i = index of single point in a brush stroke
-            for (var i = 1; i < brushPoints.length; i++) {
-                ctx.lineTo(brushPoints[i].x, brushPoints[i].y);
-                ctx.stroke();
-            }
-            ctx.closePath();
-        }
+        requestAnimationFrame(redrawCanvas);
     }
     function finishBrushStroke() {
         brushIsDrawing = false;
-        drawnObjects.push(new BrushStroke(brushPoints));
+        brushPoints.forEach(function (point) {
+            point.x /= zoom;
+            point.y /= zoom;
+        });
+        drawnObjects.push(new DrawnObject(new BrushStroke(brushPoints), pageNumber));
         brushPoints = [];
+    }
+    // 1 = original, 2 = my edit, 3 = solution
+    function switchVersion(v) {
+        version = v;
+        setImgSrc();
+    }
+    function setImgSrc() {
+        console.info('version ' + version);
+        if (version == 1)
+            image.src = 'img/' + pageNumber.toString() + '.png';
+        if (version == 2)
+            image.src = 'img/' + pageNumber.toString() + '.png';
+        if (version == 3)
+            image.src = 'img/' + pageNumber.toString() + '_r.png';
     }
 };
 // switch (tool) {
--- src/canvas.ts
+++ src/canvas.ts
@@ -1,3 +1,7 @@
+class DrawnObject {
+  constructor(public obj: any, public pageNumber: number) {
+  }
+}
 class Point {
   constructor(public x: number, public y: number) {
   }
@@ -19,46 +23,53 @@ var MAX_BRUSH_POINT_COUNT = 100
 var cvWidth = 600
 var cvHeight = 800
 var tool = 0 // 0 = none, 1 = line, 2 = text
-var drawnObjects = new Array<any>()
+var version = 2 // 1 = original, 2 = my edit, 3 = solution
+var zoom = 1
+var drawnObjects = new Array<DrawnObject>()
 var newLine = new Line(new Boundaries(new Point(0, 0), new Point(0, 0)), false)
 var brushIsDrawing = false
 var brushPoints = new Array<Point>()
 var brushLastPoint = new Point(0, 0)
 var brushCurrentPoint = new Point(0, 0)
 
-var imgOrig = new Image()
+var image = new Image()
 var imgDone = new Image()
 var imgUsr = new Image()
 var pageNumber = 1
 
-function lineClick() {
-  setTool(1)
-}
-function brushClick() {
-  setTool(2)
-}
-function textClick() {
-  setTool(3)
-  console.info({ drawnObjects })
-}
-
 function setTool(t: number) {
   tool = t
+}
+function zoomCanvas(z: number) {
+  zoom += z
   let ispan = document.getElementById('i-tool')
-  if (ispan) ispan.innerHTML = t.toString()
+  if (ispan) ispan.innerHTML = 'zoom ' + zoom.toString()
 }
 
 window.onload = function () {
   setTool(0)
+  setImgSrc()
   const cv = <HTMLCanvasElement>document.getElementById('canvas')
   const ctx = cv.getContext("2d")
   const inputPageNumber = <HTMLInputElement>document.getElementById('inputPageNumber')
+  const btnPrevPage = <HTMLButtonElement>document.getElementById('btnPrevPage')
+  const btnNextPage = <HTMLButtonElement>document.getElementById('btnNextPage')
+  const lblSwitch1 = <HTMLLabelElement>document.getElementById('lblSwitch1')
+  const lblSwitch2 = <HTMLLabelElement>document.getElementById('lblSwitch2')
+  const lblSwitch3 = <HTMLLabelElement>document.getElementById('lblSwitch3')
+  inputPageNumber.value = pageNumber.toString()
   cv.addEventListener("mousedown", handleMouseDown)
   cv.addEventListener("mouseup", handleMouseUp)
   cv.addEventListener("mousemove", handleMouseMove)
-  this.imgOrig.src = 'img/' + pageNumber.toString() + '.png'
 
-  inputPageNumber.addEventListener("keyup", function(event) {
+  // first call, calls request animation frame inside co it cycles inside after this one call
+  redrawCanvas()
+
+  inputPageNumber.addEventListener("change", function (event) {
+    pageNumber = +inputPageNumber.value
+    setImgSrc()
+  })
+  inputPageNumber.addEventListener("keyup", function (event) {
     console.info('input click')
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
@@ -66,44 +77,51 @@ window.onload = function () {
       console.info('input enter click')
       event.preventDefault();
       pageNumber = +inputPageNumber.value
-      redrawCanvas()
     }
-  }); 
+  })
+  lblSwitch1.addEventListener("click", function (event) {
+    switchVersion(1)
+  })
+  lblSwitch2.addEventListener("click", function (event) {
+    switchVersion(2)
+  })
+  lblSwitch3.addEventListener("click", function (event) {
+    switchVersion(3)
+  })
 
   // button prev page click must be async to redraw image after a small delay, otherwise it does not redraw
-  async function prevPage() {
-    cv.focus()
+  function prevPage() {
     pageNumber = --pageNumber
-    imgOrig.src = 'img/' + pageNumber.toString() + '.png'
-    console.info('page: ' + pageNumber)
-    // sleep for  10ms so the picture redraws
-    await new Promise(r => setTimeout(r, 50));
-    redrawCanvas()
+    if (pageNumber < 1) {
+      pageNumber = 1
+      btnPrevPage.disabled = true
+    }
+    setImgSrc()
     inputPageNumber.value = pageNumber.toString()
   }
   var prevbtn = document.getElementById('btnPrevPage')
   if (prevbtn) prevbtn.onclick = prevPage
 
   // button next page click must be async to redraw image after a small delay, otherwise it does not redraw
-  async function nextPage() {
-    cv.focus()
+  function nextPage() {
     pageNumber = ++pageNumber
-    imgOrig.src = 'img/' + pageNumber.toString() + '.png'
-    console.info('page: ' + pageNumber)
-    // sleep for  10ms so the picture redraws
-    await new Promise(r => setTimeout(r, 50));
-    redrawCanvas()
+    if (pageNumber == 2) {
+      btnPrevPage.disabled = false
+    }
+    setImgSrc()
     inputPageNumber.value = pageNumber.toString()
   }
   var nextbtn = document.getElementById('btnNextPage')
   if (nextbtn) nextbtn.onclick = nextPage
 
   function handleMouseDown(e: MouseEvent) {
+    // do stuff only when version 2 is active (moje upravy)
+    if (version != 2) return
     const rect = cv.getBoundingClientRect()
     switch (tool) {
       case 0: return;
       case 1:
-        newLine = new Line(new Boundaries(new Point(e.clientX - rect.left, e.clientY - rect.top), new Point(0, 0)), true)
+        newLine = new Line(new Boundaries(new Point(e.clientX - rect.left, e.clientY - rect.top), new Point(e.clientX - rect.left, e.clientY - rect.top)), true)
         break;
       case 2:
         brushPoints.push(new Point(e.clientX - rect.left, e.clientY - rect.top))
@@ -115,20 +133,25 @@ window.onload = function () {
   }
 
   function handleMouseUp(e: MouseEvent) {
+    // do stuff only when version 2 is active (moje upravy)
+    if (version != 2) return
     switch (tool) {
       case 0: return;
       case 1:
         const rect = cv.getBoundingClientRect()
-        newLine.bo.b.x = e.clientX - rect.left
-        newLine.bo.b.y = e.clientY - rect.top
+        // recalculate original point
+        newLine.bo.a.x /= zoom
+        newLine.bo.a.y /= zoom
+        // set end point
+        newLine.bo.b.x = (e.clientX - rect.left) / zoom
+        newLine.bo.b.y = (e.clientY - rect.top) / zoom
         newLine.editing = false
         // if line is too short, do not create it
         if (Math.abs(newLine.bo.a.x - newLine.bo.b.x) > 3 || Math.abs(newLine.bo.a.y - newLine.bo.b.y) > 3) {
           // object assign to create new object and not use the same refference over and over
-          drawnObjects.push(newLine)
+          drawnObjects.push(new DrawnObject(newLine, pageNumber))
           newLine = new Line(new Boundaries(new Point(0, 0), new Point(0, 0)), false)
         }
-        redrawCanvas()
         break;
       case 2:
         if (brushIsDrawing) finishBrushStroke()
@@ -137,13 +160,14 @@ window.onload = function () {
   }
 
   function handleMouseMove(e: MouseEvent) {
+    // do stuff only when version 2 is active (moje upravy)
+    if (version != 2) return
     const rect = cv.getBoundingClientRect()
     switch (tool) {
       case 0: return
       case 1:
         newLine.bo.b.x = e.clientX - rect.left
         newLine.bo.b.y = e.clientY - rect.top
-        redrawCanvas()
         break
       case 2:
         if (!brushIsDrawing) break;
@@ -155,73 +179,99 @@ window.onload = function () {
           brushCurrentPoint = new Point(e.clientX - rect.left, e.clientY - rect.top)
         }
         if (brushPoints.length >= MAX_BRUSH_POINT_COUNT) finishBrushStroke()
-        redrawCanvas()
         break
     }
   }
 
   function redrawCanvas() {
-    if (!ctx) return;
+    if (!ctx) return
+    cv.width = image.width * zoom
+    cv.height = image.height * zoom
     ctx.clearRect(0, 0, cv.width, cv.height)
 
-    ctx.drawImage(imgOrig, 0, 0, cv.width, cv.height)
+    ctx.drawImage(image, 0, 0, cv.width, cv.height)
 
     ctx.lineCap = "round"
     ctx.lineJoin = "round"
 
-    // draw objects
-    drawnObjects.forEach(object => {
-      if (object instanceof Line) {
-        ctx.lineWidth = 2
-        ctx.strokeStyle = "black"
+    // draw all objects only if version 2 is active (moje upravy)
+    if (version == 2) {
+      // draw objects
+      drawnObjects.forEach(object => {
+        if (object.pageNumber != pageNumber) return
+        if (object.obj instanceof Line) {
+          ctx.lineWidth = 2 * zoom
+          ctx.strokeStyle = "black"
+          ctx.beginPath()
+          ctx.moveTo(object.obj.bo.a.x * zoom, object.obj.bo.a.y * zoom)
+          ctx.lineTo(object.obj.bo.b.x * zoom, object.obj.bo.b.y * zoom)
+          ctx.stroke()
+          ctx.closePath()
+        } else if (object.obj instanceof BrushStroke) {
+          ctx.lineWidth = 10 * zoom
+          ctx.strokeStyle = "red"
+          ctx.beginPath()
+          ctx.moveTo(object.obj.points[0].x * zoom, object.obj.points[0].y * zoom)
+          // i = index of single point in a brush stroke
+          for (let i = 1; i < object.obj.points.length; i++) {
+            ctx.lineTo(object.obj.points[i].x * zoom, object.obj.points[i].y * zoom)
+            ctx.stroke()
+          }
+          ctx.closePath()
+        }
+      });
+
+      // draw current line
+      ctx.lineWidth = 2 * zoom
+      ctx.strokeStyle = "black"
+      ctx.beginPath()
+      if (newLine.editing) {
+        ctx.moveTo(newLine.bo.a.x, newLine.bo.a.y)
+        ctx.lineTo(newLine.bo.b.x, newLine.bo.b.y)
+        ctx.stroke();
+      }
+      // draw current brush
+      ctx.lineWidth = 10 * zoom
+      ctx.strokeStyle = "red"
+      if (brushIsDrawing) {
         ctx.beginPath()
-        ctx.moveTo(object.bo.a.x, object.bo.a.y)
-        ctx.lineTo(object.bo.b.x, object.bo.b.y)
-        ctx.stroke()
-        ctx.closePath()
-      } else if (object instanceof BrushStroke) {
-        ctx.lineWidth = 10
-        ctx.strokeStyle = "red"
-        ctx.beginPath()
-        ctx.moveTo(object.points[0].x, object.points[0].y)
+        ctx.moveTo(brushPoints[0].x, brushPoints[0].y)
         // i = index of single point in a brush stroke
-        for (let i = 1; i < object.points.length; i++) {
-          ctx.lineTo(object.points[i].x, object.points[i].y)
+        for (let i = 1; i < brushPoints.length; i++) {
+          ctx.lineTo(brushPoints[i].x, brushPoints[i].y)
           ctx.stroke()
         }
         ctx.closePath()
       }
-    });
+    }
 
-    // draw current line
-    ctx.lineWidth = 2
-    ctx.strokeStyle = "black"
-    ctx.beginPath()
-    if (newLine.editing) {
-      ctx.moveTo(newLine.bo.a.x, newLine.bo.a.y)
-      ctx.lineTo(newLine.bo.b.x, newLine.bo.b.y)
-      ctx.stroke();
-    }
-    // draw current brush
-    ctx.lineWidth = 10
-    ctx.strokeStyle = "red"
-    if (brushIsDrawing) {
-      ctx.beginPath()
-      ctx.moveTo(brushPoints[0].x, brushPoints[0].y)
-      // i = index of single point in a brush stroke
-      for (let i = 1; i < brushPoints.length; i++) {
-        ctx.lineTo(brushPoints[i].x, brushPoints[i].y)
-        ctx.stroke()
-      }
-      ctx.closePath()
-    }
+
+    requestAnimationFrame(redrawCanvas)
   }
 
   function finishBrushStroke() {
     brushIsDrawing = false;
-    drawnObjects.push(new BrushStroke(brushPoints))
+    brushPoints.forEach(point => {
+      point.x /= zoom
+      point.y /= zoom
+    })
+    drawnObjects.push(new DrawnObject(new BrushStroke(brushPoints), pageNumber))
     brushPoints = []
   }
+
+  // 1 = original, 2 = my edit, 3 = solution
+  function switchVersion(v: number) {
+    version = v
+    setImgSrc()
+  }
+
+  function setImgSrc() {
+    console.info('version ' + version)
+    if (version == 1) image.src = 'img/' + pageNumber.toString() + '.png'
+    if (version == 2) image.src = 'img/' + pageNumber.toString() + '.png'
+    if (version == 3) image.src = 'img/' + pageNumber.toString() + '_r.png'
+  }
+
 };
 // switch (tool) {
 //   case 0: return;
